bb549e869a7a39f9d277dcd54498f4caa636d881
Photified <justinmainphoto@gmail.com>
2026-05-05 13:32:29 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'pocket-pulls-v7'; // Bumped to v7!
+const CACHE_NAME = 'pocket-pulls-v8';
 const ASSETS_TO_CACHE = [
     './',
     './index.html',
@@ -9,12 +9,31 @@ const ASSETS_TO_CACHE = [
     './logo.png'
 ];
 
+// Install event: Cache files and force the new worker to take over
 self.addEventListener('install', event => {
+    self.skipWaiting(); // Bypasses the "waiting" state
     event.waitUntil(
         caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS_TO_CACHE))
     );
 });
 
+// Activate event: Delete old, broken caches
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cache => {
+                    if (cache !== CACHE_NAME) {
+                        console.log('Deleting old cache:', cache);
+                        return caches.delete(cache);
+                    }
+                })
+            );
+        }).then(() => self.clients.claim()) // Takes control of the open pages immediately
+    );
+});
+
+// Fetch event: Serve cached files if offline
 self.addEventListener('fetch', event => {
     event.respondWith(
         caches.match(event.request).then(response => {
